fdba2e3e9d7311bd546761a805f015ad841d3acf
KultenFM <informatica.fams@gmail.com>
2026-07-24 15:21:59 -0400

    Configurar los models y los schemas en Pydanctic y FastAPI

---
+++ main.py
@@ -0,0 +1,66 @@
+"""
+Función de FastAPI: abrirá la conexión a la base de datos, usará los esquemas de Pydantic para validar
+la entrada y usará los modelos de SQLAlchemy para guardar la información.
+"""
+# 📌 Importamos los módulos necesarios para la aplicacion FastAPI
+import models
+import schemas
+from database import SessionLocal
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
+
+# 🛠️  Creamos la aplicación FastAPI
+# Motor de la aplicación
+app = FastAPI(title="API Tablero D&D - Dungeon Master")
+
+# 🛠️ Funcion: Gestor que abre una sesión cada vez que llega una petición y la cierra al terminar
+def get_db():
+    db = SessionLocal()
+    try:
+        # yield permite que la función devuelva un valor y luego continúe ejecutándose después de que se haya usado ese valor.
+        # En este caso, devuelve la sesión de la base de datos para que pueda ser utilizada en las rutas de FastAPI
+        yield db
+    finally:
+        db.close()
+
+# ▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️
+# ==========================================
+# 📝 RUTAS DE LA API (ENDPOINTS)
+# ==========================================
+
+# ☝️ Definimos la puerta por la cual debe entrar la información
+# response_model indica que la respuesta de esta ruta será del tipo Monstruo definido en schemas.py
+# para asegurarnos de que la salida tenga el formato correcto
+@app.post("/monstruos/", response_model=schemas.Monstruo)
+
+# 🛠️ Función 1: Crear un monstruo por parte del usuario e insetarlo en la base de datos
+def crear_monstruos(monstruo: schemas.MonstruoCreate, db: Session = Depends(get_db)):
+    # 📌 Convertimos los datos validados de Pydantic a un objeto de SQLAlchemy (Traducción)
+    nuevo_monstruo = models.Monstruo(**monstruo.model_dump())
+
+    # 📌 Guardamos en la base de datos
+    db.add(nuevo_monstruo)      # Agrega el nuevo monstruo a la sesión de la base de datos
+    db.commit()                 # Guarda los cambios en la base de datos, lo que inserta el nuevo monstruo en la tabla correspondiente
+    db.refresh(nuevo_monstruo)  # Actualiza el objeto nuevo_monstruo con los datos de la base de datos, incluyendo el ID generado automáticamente
+
+    return nuevo_monstruo
+
+# ▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️
+
+# ☝️ Definimos la puerta por la cual debe entrar la información
+# Get indica que esta ruta responderá a solicitudes para obtener datos del servidor.
+# response_model=list[schemas.Monstruo] indica que la respuesta de esta ruta será una lista de objetos del tipo Monstruo definido en schemas.py
+@app.get("/monstruos/", response_model=list[schemas.Monstruo])
+
+# 🛠️ Función 2: 🔍 Buscar todos los monstruos con el método GET en la base de datos
+def buscar_monstruos(db: Session = Depends(get_db)):
+
+    # 📌 1. Consultamos todos los monstruos directamente usando la sesión inyectada `db`
+    lista_monstruos = db.query(models.Monstruo).all()
+
+    # 📌 2. Se entrega la información al visitante
+    return lista_monstruos
+
+# ▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️
+
+
---
+++ schemas.py
@@ -0,0 +1,41 @@
+"""
+Esquemas para los monstruos en el juego de Dungeons & Dragons (DnD).
+Estos esquemas definen la estructura de los datos para los monstruos, incluyendo sus atributos, iniciativa y ataque base.
+Se utilizan para la validación de datos y la serialización/deserialización en la aplicación
+"""
+
+from pydantic import BaseModel
+
+# ==========================================
+# 🐉 ESQUEMAS PARA LOS MONSTRUOS
+# ==========================================
+
+#🛠️ Se crea el modelo de validación de los datos
+class MonstruoBase(BaseModel):
+    nombre_monstruo: str
+    iniciativa : int = 10
+    fuerza : int = 10
+    destreza : int = 10
+    constitucion : int = 10
+    inteligencia : int = 10
+    sabiduria : int = 10
+    carisma : int = 10
+    ataque_base : int = 0
+
+# 🛠️ Se hereda el modelo para la creación de un monstruo
+class MonstruoCreate(MonstruoBase):
+    pass
+
+# 🐉 Se hereda el modelo para la representación de un monstruo con su ID
+class Monstruo(MonstruoBase):
+    id: int
+
+    #💡 Se agrega la configuración para permitir la creación de instancias a partir de atributos
+    class Config:
+        from_attributes = True
+
+
+
+
+
+
